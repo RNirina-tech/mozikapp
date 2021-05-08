f3977efdd54270bcb88a3f59310c0b4fe2deb9b2
Nirina Ratovomanana <aurelien.ratovomanana31@gmail.com>
2021-05-08 17:28:34 +0200

    Ajout des routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
+import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/Navbar/NavBar';
 import Purchase from './components/Pages/Purchase';
 import Home from './components/Pages/Home';
 
-function App() {
-  return (
-    <section id="todo">
+const App = () => (
+  <section id="todo">
       <BrowserRouter>
+        <NavBar />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Home}/>
           <Route exact path="/purchase" component={Purchase} />
         </Switch>
-        <NavBar />
       </BrowserRouter>
     </section>
-  )
-}
+)
 
 export default App;
