11bfeae7cac7143c459abd57fa4efaa81b358126
Nirina Ratovomanana <aurelien.ratovomanana31@gmail.com>
2021-05-08 17:29:26 +0200

    Finalisation du navbat / Modal crédit à ajouter

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,5 +1,6 @@
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Credit from '../Modal/Credits';
 import './NavBar.css';
 
 const { Brand, Collapse } = Navbar;
@@ -7,7 +8,7 @@ const { Brand, Collapse } = Navbar;
 const NavBar = () => (
     <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
         <Brand as={Link} to="/">
-            <img className="logo" src="https://uploads-ssl.webflow.com/5e21dacb31c76fac21d9a660/5fb397a581782a26d328a58a_Logo.png" />
+            <img className="logo" src="https://uploads-ssl.webflow.com/5e21dacb31c76fac21d9a660/5fb397a581782a26d328a58a_Logo.png" alt="logo merci yanis"/>
         </Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Collapse id="responsive-navbar-nav">
